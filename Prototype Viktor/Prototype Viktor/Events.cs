ddf09f70941bd1279bd04387d17e388c227e6a8b
Autechre777 <Autechre777@gmail.com>
2016-06-23 07:49:58 -0700

    Update Events.cs

--- Prototype Viktor/Prototype Viktor/Events.cs
+++ Prototype Viktor/Prototype Viktor/Events.cs
@@ -26,19 +26,13 @@ namespace Prototype_Viktor
         private static void Orbwalker_OnPreAttack(AttackableUnit target, Orbwalker.PreAttackArgs args)
         {
             var a = target as Obj_AI_Minion;
-            var allys = EntityManager.Heroes.Allies.Count(c => Player.Instance.Distance(c) <= Player.Instance.AttackRange);
 
             if (a == null)
             {
                 return;
             }
 
-            if (allys < 1)
-            {
-                return;
-            }
-
-            if (Settings.DisableMAA)
+            if (a !== null)
             {
                 args.Process = false;
             }
