778e3aa598100644a99dfb8dbcb9072e157d9480
Autechre777 <Autechre777@gmail.com>
2016-06-23 07:37:11 -0700

    Update Events.cs

--- Prototype Viktor/Prototype Viktor/Events.cs
+++ Prototype Viktor/Prototype Viktor/Events.cs
@@ -7,10 +7,7 @@ using EloBuddy.SDK.Events;
 using EloBuddy.SDK.Rendering;
 using SharpDX;
 
-using Settings = Bard.Config.Modes.Misc;
-using Settings2 = Bard.Config.Draw.DrawMenu;
-
-namespace Bard
+namespace MinionAA
 {
     public static class Events
     {
@@ -28,66 +25,6 @@ namespace Bard
         {
 
         }
-
-
-
-        private static void OnDraw(EventArgs args)
-        {
-            if (Settings2.DrawQ && SpellManager.Q.IsLearned)
-            {
-                Circle.Draw(Color.Green, SpellManager.Q.Range, Player.Instance.Position);
-            }
-
-            if (Settings2.DrawW && SpellManager.W.IsLearned)
-            {
-                Circle.Draw(Color.Red, SpellManager.W.Range, Player.Instance.Position);
-            }
-
-            if (Settings2.DrawR && SpellManager.R.IsLearned)
-            {
-                Circle.Draw(Color.DarkBlue, SpellManager.R.Range, Player.Instance.Position);
-            }
-
-            if (SpellManager.HasSmite())
-            {
-                if (Settings2.DrawSmite && Config.Smite.SmiteMenu.SmiteToggle
-                    || Settings2.DrawSmite && Config.Smite.SmiteMenu.SmiteCombo
-                    || Settings2.DrawSmite && Config.Smite.SmiteMenu.SmiteEnemies)
-                {
-                    Circle.Draw(Color.Blue, SpellManager.Smite.Range, Player.Instance.Position);
-                }
-            }
-        }
-
-        private static void OnGapCloser(AIHeroClient sender, Gapcloser.GapcloserEventArgs e)
-        {
-            if (sender == null || sender.IsAlly || !Settings.QGapcloser)
-            {
-                return;
-            }
-            var gapclosepred = SpellManager.Q.GetPrediction(sender);
-            if (SpellManager.Q.IsReady() && SpellManager.Q.IsInRange(sender) && e.End.Distance(Player.Instance) <= 300)
-            {
-                SpellManager.Q.Cast(gapclosepred.CastPosition);
-            }
-        }
-
-        private static void Interrupter_OnInterruptableSpell(Obj_AI_Base sender, Interrupter.InterruptableSpellEventArgs e)
-        {
-            if (sender == null || sender.IsAlly || !Settings.RInterrupt)
-            {
-                return;
-            }
-
-            if (SpellManager.R.IsReady() && SpellManager.R.IsInRange(sender) && e.DangerLevel == DangerLevel.High)
-            {
-                Core.DelayAction(delegate
-                {
-                    SpellManager.R.Cast(sender);
-                }, Settings.RInterruptDelay);
-            }
-        }
-
 
         private static void Orbwalker_OnPreAttack(AttackableUnit target, Orbwalker.PreAttackArgs args)
         {
